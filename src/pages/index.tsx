ec9b0376f7932f63ea0aad56fa8597322f68b5e7
highgoal215 <highgoal215@gmail.com>
2025-03-01 12:00:34 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,7 +58,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our documents
           </a>
         </div>
       </main>
